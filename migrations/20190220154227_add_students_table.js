7e61196370e1704fb584c49256ebff4874e425ca
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-20 15:49:01 -0500

    Create database and initiate migrations for cohorts and students tables

---
+++ migrations/20190220154227_add_students_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("students", function(table) {
+    table.increments();
+    table.string("name", 128).notNullable();
+    table
+      .integer("cohort_id")
+      .unsigned()
+      .references("id")
+      .inTable("cohorts")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("students");
+};
