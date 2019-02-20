ebdb662bf56609a56b558ae208366d812d34d5c4
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-20 16:41:39 -0500

    Successful DELETE on /api/cohorts/:id

--- routes/cohortsRouter.js
+++ routes/cohortsRouter.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const db = knex(knexConfig.development);
 
 const errors = {
-    '19':'A record with that name already exists.'
+    '19':'A record with that name already exists.',
+    'default_error':"There was an error."
 }
 
 // POST to /api/cohorts
@@ -15,7 +16,7 @@ router.post('/', async (req, res) => {
         const [id] = await db('cohorts').insert(req.body);
         res.status(201).json(id);
     } catch (error) {
-        const errorMsg = errors[error.errno] || "There was an error.";
+        const errorMsg = errors[error.errno] || errors[default_error];
         res.status(500).json(errorMsg);
     }
 })
@@ -26,7 +27,8 @@ router.get('/', async (req, res) => {
         const cohorts = await db('cohorts');
         res.status(200).json(cohorts);
     } catch (error) {
-        res.status(500).json(error);
+        const errorMsg = errors[error.errno] || errors[default_error];
+        res.status(500).json(errorMsg);
     }
 })
 
@@ -41,7 +43,8 @@ router.get('/:id', async (req, res) => {
             res.status(404).json({error: `A cohort with the ID of ${req.params.id} does not exist.`});
         }
     } catch (error) {
-        res.status(500).json(error);
+        const errorMsg = errors[error.errno] || errors[default_error];
+        res.status(500).json(errorMsg);
     }
 })
 
@@ -66,16 +69,23 @@ router.put('/:id', async (req, res) => {
             res.status(404).json({error: `A cohort with the ID of ${req.params.id} does not exist.`});
         }
     } catch (error) {
-        res.status(500).json(error);
+        const errorMsg = errors[error.errno] || errors[default_error];
+        res.status(500).json(errorMsg);
     }
 })
 
 // DELETE to /api/cohorts/:id
 router.delete('/:id', async (req, res) => {
     try {
-
+        const cohort = await db('cohorts').where({id: req.params.id}).del();
+        if (cohort > 0) {
+            res.status(204).send("The record was successfully deleted.");
+        } else {
+            res.status(404).json({error: `The cohort with the ID of ${req.params.id} does not exist.`})
+        }
     } catch (error) {
-        
+        const errorMsg = errors[error.errno] || errors[default_error];
+        res.status(500).json(errorMsg);
     }
 })
 
